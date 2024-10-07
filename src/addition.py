961f042dee7cca62246dd71b293bb89b8d66436c
Karthik Selvaraj <45594018+skarthiselvaraj@users.noreply.github.com>
2024-10-07 18:22:49 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -7,3 +7,11 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+#Substract value
+def subtract(a, b):
+    return a - b
+    
+def test_subtract():
+    assert subtract(5, 99) == -94  # Corrected expected value
+    assert subtract(99, 6) == 93   # Corrected expected value
